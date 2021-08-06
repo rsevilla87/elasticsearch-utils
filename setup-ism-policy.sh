01ac9fa6b7e1ec32e15654808a6a12210e6564d8
Raul Sevilla <rsevilla@redhat.com>
2021-08-06 12:28:41 +0200

    Update to _index_template API
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- setup-ism-policy.sh
+++ setup-ism-policy.sh
@@ -5,7 +5,7 @@
 usage(){
   cat << EOF
 Invocation error, usage:
-$0 <ES_INSTANCE> <ES_INDEX> <POLICY>
+$0 <ES_INSTANCE> <ES_INDEX> <DATE_FIELD>
 EOF
   exit 1
 }
@@ -15,23 +15,31 @@ if [[ ${#} -ne 3 ]]; then
 fi
 ES_SERVER=$1
 INDEX=$2
-POLICY=$3
+DATE_FIELD=$3
 
 # Create template with the desired ISM policy
-echo "Creating template ${INDEX}"
-curl -sS -X PUT ${ES_SERVER}/_template/${INDEX} -H "Content-type: application/json" -d '{
-  "index_patterns": [
-    "'${INDEX}'-*"
-  ],
-  "settings": {
-    "opendistro.index_state_management.policy_id": "'${POLICY}'",
-    "opendistro.index_state_management.rollover_alias": "'${INDEX}'",
-    "index":{
+TEMPLATE='{
+  "index_patterns": ["'${INDEX}'-*"],
+  "template": {
+    "settings": {
+      "opendistro.index_state_management.rollover_alias": "'${INDEX}'",
       "number_of_replicas": 0
+    },
+    "mappings": {
+      "properties": {
+        "'${DATE_FIELD}'": {
+          "type": "date"
+        }
+      }
     }
   }
 }'
+echo ${TEMPLATE} | jq .
+read -p "Hit enter to create the previous _index_template"
+echo "Creating template ${INDEX}"
+echo ${TEMPLATE} | curl -sS -X PUT ${ES_SERVER}/_index_template/${INDEX} -H "Content-type: application/json" -d@-
 
+read -p "Hit enter to reindex"
 # Reindex
 echo "Reindexing ${INDEX} to ${INDEX}-000001"
 curl -sS -X POST ${ES_SERVER}/_reindex -H "Content-type: application/json" -d '{
@@ -43,10 +51,12 @@ curl -sS -X POST ${ES_SERVER}/_reindex -H "Content-type: application/json" -d '{
   }
 }'
 
+read -p "Hit enter to delete original index"
 # Delete original index
 echo "Deleting ${INDEX}"
 curl -sS -X DELETE ${ES_SERVER}/${INDEX}
 
+read -p "Hit enter to create alias"
 # Create alias
 echo "Creating alias ${INDEX} for ${INDEX}-000001"
 curl -sS -X POST ${ES_SERVER}/_aliases -H "Content-type: application/json" -d '{
